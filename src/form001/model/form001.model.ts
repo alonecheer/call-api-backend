4b131c47b737e77677189b8962a08a39bf91996e
alonecheer <alonecheer@hotmail.com>
2020-03-09 16:08:51 +0700

    add model form001

---
+++ src/form001/model/form001.model.ts
@@ -0,0 +1,104 @@
+import {
+    AllowNull,
+    Column,
+    Model,
+    PrimaryKey,
+    Table,
+    AutoIncrement,
+} from 'sequelize-typescript';
+
+@Table({
+    timestamps: true,
+    paranoid: true,
+})
+export class Book extends Model<Book> {
+    @AllowNull(false)
+    @PrimaryKey
+    @AutoIncrement
+    @Column
+    o_id: number;
+
+    @AllowNull(false)
+    @Column
+    o_no: string;
+
+    @AllowNull(false)
+    @Column
+    o_date: string;
+
+    @AllowNull(false)
+    @Column
+    o_year: string;
+
+    @AllowNull(false)
+    @Column
+    o_purpose: string;
+
+    @AllowNull(false)
+    @Column
+    o_committee1: string;
+
+    @AllowNull(false)
+    @Column
+    o_committee2: string;
+
+    @AllowNull(false)
+    @Column
+    o_committee3: string;
+
+    @AllowNull(false)
+    @Column
+    o_numdoc: number;
+
+    @AllowNull(false)
+    @Column
+    o_pricedoc: number;
+
+    @AllowNull(false)
+    @Column
+    o_committee4: string;
+
+    @AllowNull(false)
+    @Column
+    o_committee5: string;
+
+    @AllowNull(false)
+    @Column
+    o_committee6: string;
+
+    @AllowNull(false)
+    @Column
+    o_budgettype: string;
+
+    @AllowNull(false)
+    @Column
+    o_budgetname: string;
+
+    @AllowNull(false)
+    @Column
+    o_budgetdoc: number;
+
+    @AllowNull(false)
+    @Column
+    o_approve: string;
+
+    @AllowNull(false)
+    @Column
+    o_approver: string;
+
+    @AllowNull(false)
+    @Column
+    staff_id: string;
+
+    @AllowNull(false)
+    @Column
+    o_status: string;
+
+    @AllowNull(false)
+    @Column
+    o_status_id: number;
+
+    @AllowNull(false)
+    @Column
+    o_user: string;
+}
