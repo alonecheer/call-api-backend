0a1c209fd9863732f4908fe7589fda7ed5f143b3
alonecheer <alonecheer@hotmail.com>
2020-03-09 19:41:13 +0700

    add form001 service

--- src/form001/form001.service.ts
+++ src/form001/form001.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,Inject } from '@nestjs/common';
+import { Form001 } from './model/form001.model';
 
 @Injectable()
-export class Form001Service {}
+export class Form001Service {
+    constructor(@Inject('form001Repo') private readonly form001: typeof Form001){}
+
+    async getForm001(){
+        return await this.form001.findAll();
+    }
+}
